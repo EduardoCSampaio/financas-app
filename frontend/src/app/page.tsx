b1f9ca0112517532b9eaf11eaf23cf7c79a94a9c
EduardoCSampaio <sampaioeduardocamposs@gmail.com>
2025-06-30 15:36:58 -0300

    tey tey tey

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,6 +14,7 @@ import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
 import Link from 'next/link';
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, LineChart, CartesianGrid, XAxis, YAxis, Line } from 'recharts';
+import { saveAs } from 'file-saver';
 
 // Spinner de carregamento
 function Spinner() {
